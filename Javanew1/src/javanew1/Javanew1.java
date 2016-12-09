00fa9e22aa9969c428f5facb34ab8485458b2ec8
gittyRavi <rairavisaxena@gmail.com>
2016-12-09 21:38:41 +0530

    Add files via upload

---
+++ Javanew1/src/javanew1/Javanew1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanew1;
+
+/**
+ *
+ * @author ravi
+ */
+public class Javanew1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello I am java.\n");
+    }
+    
+}
